184deceba3fd03e72c4eadacb54c7c9da6c20339
uint <abhinav.prsai@gmail.com>
2025-12-14 15:22:57 +0000

    add opening of display

--- xnap.c
+++ xnap.c
@@ -1,6 +1,35 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <X11/Xlib.h>
+
+void die(const char* s);
+void quit(void);
+void xsetup(void);
+
+Display* dpy = NULL;
+Window root = None;
+int scr = -1;
+
+void die(const char* s)
+{
+	fprintf(stderr, "xnap: %s", s);
+	exit(1);
+}
+
+void quit(void)
+{
+	XCloseDisplay(dpy);
+}
+
+void xsetup(void)
+{
+	if (!(dpy = XOpenDisplay(NULL)))
+		die ("failed to open display");
+
+	int scr = DefaultScreen(dpy);
+	root = RootWindow(dpy, scr);
+}
 
 int main(void)
 {
