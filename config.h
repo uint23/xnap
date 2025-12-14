3f455db8ad07ca1cba56ad02ebb5f17368d346cc
uint <abhinav.prsai@gmail.com>
2025-12-14 22:04:46 +0000

    have sane defaults + rip gumby
    packaging killed gumby </3 RIP

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 #include <X11/cursorfont.h>
 
-static const unsigned int sel_cursor = XC_gumby;
+static const unsigned int sel_cursor = XC_plus;
 static const unsigned int win_cursor = XC_right_ptr;
-static const unsigned int sel_line_width = 5;
+static const unsigned int sel_line_width = 2;
